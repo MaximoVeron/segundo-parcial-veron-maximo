d7e6fcd5d6c3e3cc53b2670466880b635316309b
Veron Maximo <maximoveron49@gmail.com>
2025-06-27 10:57:28 -0300

    estructuracion de los modelos en movie.model.js

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path'
+import router from './routes/pelicula.routes.js'; 
+import { initDB } from './config/db.js'; 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-// app.use("/api/pelicula", router);
+app.use("/api/pelicula", router);
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
+initDB().then(() => {
+    app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    });
 });
---
+++ src/models/movie.model.js
@@ -0,0 +1,22 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.js"
+
+const Movie = sequelize.define("Movie", {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+    },
+});
+
+export default Movie;
